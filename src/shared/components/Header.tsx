48c2c4af3f4034e614cd947220d12398cd1daacb
DESKTOP-PSE0Q3Q\Alex <alexvasuni@gmail.com>
2025-11-10 23:46:55 -0600

    Update Header component title to reflect version change from v1 to v2

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -10,7 +10,7 @@ function Header({ className = '' }: HeaderProps) {
       <div className="flex items-center h-16">
         <Link to="/" className="flex items-center">
           <h1 className="text-xl font-bold text-white dark:text-white pl-4">
-            Finance Tracker v1
+            Finance Tracker v2
           </h1>
         </Link>
       </div>
